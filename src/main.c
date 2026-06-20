d54f32efed027f03d16623d798539d28ba874ecd
Prem Kaushik <premkaushik65@Prems-MacBook-Air.local>
2026-06-20 11:17:34 +0530

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
 
+
+
   // TODO: Uncomment the code below to pass the first stage
-  printf("$ ");
 
-  scanf("%s", argv[0]);
-
-  printf("%s: command not found", argv[0]);
-
+  while (1){
+    printf("$ ");
+    char input[100];
+    // Waiting for User input
+    fgets(input,100,stdin);
+    // Removing trailing newline from the string
+    input[strlen(input)-1] = '\0';
+    printf("%s: command not found\n", input);
+  }
   return 0;
 }
